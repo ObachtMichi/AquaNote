c26565eb8278407f6a1ea6dbd8d9118ea260bfd9
Michael Schuster <michael.schuster_1@stud.th-rosenheim.de>
2022-07-24 14:00:34 +0200

    Overhaul startscreen

--- app/src/main/java/com/example/aquanote/MainActivity.java
+++ app/src/main/java/com/example/aquanote/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.aquanote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameAquaInput;
+    private EditText volumeAquaInput;
+    private Button buttonSelectValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameAquaInput = (EditText) findViewById(R.id.nameAquaInput);
+        volumeAquaInput = (EditText) findViewById(R.id.volumeAquaInput);
+        buttonSelectValue = (Button) findViewById(R.id.buttonSelectValue);
+
+
+
+    }
+
+    public void nextActivity(View v){
+        Intent i = new Intent(this, SelectValuesStart.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/aquanote/SelectValuesStart.java
@@ -0,0 +1,14 @@
+package com.example.aquanote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectValuesStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_values_start);
+    }
+}
