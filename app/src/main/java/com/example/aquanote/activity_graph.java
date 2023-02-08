30ee30208ca242d0fdcd89a203d2b506fb977176
michael <schuastamiche@gmail.com>
2023-02-08 11:26:22 +0100

    Graph works

--- app/src/main/java/com/example/aquanote/activity_graph.java
+++ app/src/main/java/com/example/aquanote/activity_graph.java
@@ -31,6 +31,9 @@ import android.widget.Toast;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 
 import java.util.ArrayList;
@@ -50,15 +53,35 @@ public class activity_graph extends AppCompatActivity implements AdapterView.OnI
     String date1;
 
 
+
+
     //-----------------------------------------Variablen---------------------------------------------------
 
 
     //-----------------------------------------On Create---------------------------------------------------
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+
+
+
+
+
 
         btn_AddEntry = findViewById(R.id.btn_AddEntry);
         btn_AddEntry();
